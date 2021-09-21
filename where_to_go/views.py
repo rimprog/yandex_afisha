d603c68f88ac127f818437e71355a18cfba8c21a
rimprog <riminprog@gmail.com>
2021-09-21 12:41:31 +0300

    Move data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
 
+from places.models import Place
+
+
+def create_geo_json():
+    geo_json = {
+        "type": "FeatureCollection",
+        "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.62, 55.793676]
+          },
+          "properties": {
+            "title": "«Легенды Москвы",
+            "placeId": "moscow_legends",
+            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.64, 55.753676]
+          },
+          "properties": {
+            "title": "Крыши24.рф",
+            "placeId": "roofs24",
+            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json"
+          }
+        }
+        ]
+    }
+
+    return geo_json
+
 
 def show_index_page(request):
-    return render(request, 'index.html')
+    geo_json = create_geo_json()
+
+    return render(request, 'index.html', context={'geo_json':geo_json})
