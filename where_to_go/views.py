fdd61836744fa4bffd1acd6a658af33025dad454
rimprog <riminprog@gmail.com>
2021-09-21 12:55:32 +0300

    Created database output to geo_json

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -3,41 +3,35 @@ from django.shortcuts import render
 from places.models import Place
 
 
-def create_geo_json():
+def create_geo_json(places):
+    geo_json_points = []
+
+    for place in places:
+        geo_json_point = {
+            "type": "Feature",
+            "geometry": {
+            "type": "Point",
+            "coordinates": [place.longitude, place.latitude]
+            },
+            "properties": {
+            "title": place.title,
+            "placeId": place.place_id,
+            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json" if place.place_id=='moscow_legends' else "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json" 
+            }
+        }
+
+        geo_json_points.append(geo_json_point)
+
     geo_json = {
         "type": "FeatureCollection",
-        "features": [
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.62, 55.793676]
-          },
-          "properties": {
-            "title": "«Легенды Москвы",
-            "placeId": "moscow_legends",
-            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
-          }
-        },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.64, 55.753676]
-          },
-          "properties": {
-            "title": "Крыши24.рф",
-            "placeId": "roofs24",
-            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json"
-          }
-        }
-        ]
+        "features": geo_json_points
     }
 
     return geo_json
 
 
 def show_index_page(request):
-    geo_json = create_geo_json()
+    places = Place.objects.all()
+    geo_json = create_geo_json(places)
 
     return render(request, 'index.html', context={'geo_json':geo_json})
