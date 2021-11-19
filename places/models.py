db6089e210f784737d2f263718d16447fe184e85
rimprog <riminprog@gmail.com>
2021-11-19 11:52:22 +0300

    Made the title field in Place model required

--- places/models.py
+++ places/models.py
@@ -3,7 +3,7 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField('Название места', max_length=200, blank=True)
+    title = models.CharField('Название места', max_length=200)
     description_short = models.TextField('Короткое описание', blank=True)
     description_long = HTMLField('Длинное описание', blank=True)
     latitude = models.FloatField('Широта')
